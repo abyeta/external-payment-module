82603e71fe69b6e384a1b5d4bbb5d93928a20781
Joaquin Arrazola <joaquin.arrazola@jala.university>
2024-03-28 09:28:54 -0400

    Add Initial Project Structure

---
+++ src/main/java/org/jala/university/MainApp.java
@@ -0,0 +1,10 @@
+package org.jala.university;
+
+import org.jala.university.presentation.MainView;
+
+public class MainApp {
+    public static void main(String[] args) {
+        MainView.launch(args);
+    }
+
+}
