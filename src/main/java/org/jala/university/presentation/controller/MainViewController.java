7c11b27aa8d106350021a13d6a98499d1dd6c641
Jean Pierre Crespin <crespinpierre98@gmail.com>
2025-10-28 22:33:40 -0300

    #30 feat (UI): merge feature/US-30-Register-external-service into develop

--- src/main/java/org/jala/university/presentation/controller/MainViewController.java
+++ src/main/java/org/jala/university/presentation/controller/MainViewController.java
@@ -1,31 +1,31 @@
-package org.jala.university.presentation.controller;
-
-import javafx.fxml.FXML;
-import org.jala.university.commons.presentation.BaseController;
-import org.jala.university.commons.presentation.ViewSwitcher;
-import org.jala.university.presentation.ExternalPaymentView;
-
-/**
- * Controller for the main view of the External Payment Module.
- * Provides navigation to different module features.
- */
-public class MainViewController extends BaseController {
-
-    /**
-     * Navigates to the External Service Registration view.
-     * This method is called when the user clicks the "Register Service" button.
-     */
-    @FXML
-    private void onRegisterService() {
-        ViewSwitcher.switchTo(ExternalPaymentView.EXTERNAL_SERVICE_REGISTRATION.getView());
-    }
-
-    /**
-     * Navigates back to the main view.
-     * This method can be used by other views to return to the main menu.
-     */
-    @FXML
-    private void onBackToMain() {
-        ViewSwitcher.switchTo(ExternalPaymentView.MAIN.getView());
-    }
-}
+//package org.jala.university.presentation.controller;
+//
+//import javafx.fxml.FXML;
+//import org.jala.university.commons.presentation.BaseController;
+//import org.jala.university.commons.presentation.ViewSwitcher;
+//import org.jala.university.presentation.ExternalPaymentView;
+//
+///**
+// * Controller for the main view of the External Payment Module.
+// * Provides navigation to different module features.
+// */
+//public class MainViewController extends BaseController {
+//
+//    /**
+//     * Navigates to the External Service Registration view.
+//     * This method is called when the user clicks the "Register Service" button.
+//     */
+//    @FXML
+//    private void onRegisterService() {
+//        ViewSwitcher.switchTo(ExternalPaymentView.EXTERNAL_SERVICE_REGISTRATION.getView());
+//    }
+//
+//    /**
+//     * Navigates back to the main view.
+//     * This method can be used by other views to return to the main menu.
+//     */
+//    @FXML
+//    private void onBackToMain() {
+//        ViewSwitcher.switchTo(ExternalPaymentView.MAIN.getView());
+//    }
+//}
