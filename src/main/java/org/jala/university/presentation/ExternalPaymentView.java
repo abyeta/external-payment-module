65408b7ab5c188b86bccd20774f2edd7e539c036
luis barajas <luis.barajas0125@jala.university>
2025-11-08 20:22:45 -0500

    #39 feat: Make a json to simulate the API

--- src/main/java/org/jala/university/presentation/ExternalPaymentView.java
+++ src/main/java/org/jala/university/presentation/ExternalPaymentView.java
@@ -8,6 +8,7 @@ public enum ExternalPaymentView {
     START_MENU("external-service-start-view.fxml"),
     MAIN("main-menu.fxml"),
     EXTERNAL_SERVICE("external-service-view.fxml"),
+    INVOICE_DETAILS("invoice-details-view.fxml"),
     EXTERNAL_SERVICE_INVOICES("external-service-invoices-view.fxml"),
     EXTERNAL_SERVICE_REGISTRATION("external-service-registration-view.fxml"),
     EXTERNAL_SERVICE("external-service-view.fxml"),
